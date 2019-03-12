dc8f579b274139ecd49399f73a2f06ffa67776ea
Hamza The Z <Hamza The Z@HamzaTheZ-PC>
2019-03-12 14:53:46 +0000

    Begin

--- gestiondesplat.php
+++ gestiondesplat.php
@@ -1,3 +1,4 @@
+<?php ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -37,29 +38,28 @@
                 </button>
                 <div class="collapse navbar-collapse" id="ftco-nav">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a href="index.html" class="nav-link">Accueil</a></li>
-                        <li class="nav-item"><a href="menu.html" class="nav-link">Menu</a></li>
-                        <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
-                        <li class="nav-item"><a href="about.html" class="nav-link">A Propos</a></li>
-                        <li class="nav-item active"><a href="contact.html" class="nav-link">Contact</a></li>
+                        <li class="nav-item"><a href="adminIndex.php" class="nav-link">Home</a></li>
+                        <li class="nav-item active"><a href="gestiondesplat.php" class="nav-link">Gestion des Plat</a></li>
+                        <li class="nav-item"><a href="services.html" class="nav-link">Gestion des Commande</a></li>
+                        <li class="nav-item"><a href="blog.html" class="nav-link">Gestion des  Laivreur &AMP; Véhicule</a></li>
+                        <li class="nav-item"><a href="about.html" class="nav-link">Histoir</a></li>
+                        <li class="nav-item"><a href="contact.html" class="nav-link">Consulter</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
         <!-- END nav -->
 
-
         <section class="home-slider owl-carousel img" style="background-image: url(images/bg_1.jpg);">
+
             <div class="slider-item" style="background-image: url(images/bg_3.jpg);">
                 <div class="overlay"></div>
                 <div class="container">
-                    <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
+                    <div class="row slider-text justify-content-center align-items-center">
 
                         <div class="col-md-7 col-sm-12 text-center ftco-animate">
-                            <span class="subheading">Contactez Nous</span>
-                            <h1 class="mb-4">Nous sommes toujours à l'écoute</h1>
-                            <p class="mb-4 mb-md-5">Vous pouvez vous inscrire ou créer un nouveau compte et vous pouvez nous contacter directement</p>
-                            <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Créé Une Compte</a> <a href="#" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">Inscrire</a></p>
+                            <h1 class="mb-3 mt-5 bread">Ajouter Des Plat</h1>
+                            <p class="breadcrumbs"><span>Ajouter Plat</span>  <span class="mr-2"><a href="platlist.php">Voir La list</a></span> </p>
                         </div>
 
                     </div>
@@ -73,19 +73,19 @@
                     <div class="col-md-4 contact-info ftco-animate">
                         <div class="row">
                             <div class="col-md-12 mb-4">
-                                <h2 class="h4">Envoyez Votre Message</h2>
+                                <h2 class="h4">Ajouter Un Plat</h2>
                             </div>
                             <div class="col-md-12 mb-3">
-                               
+
                             </div>
                             <div class="col-md-12 mb-3">
-                            
+
                             </div>
                             <div class="col-md-12 mb-3">
-                              
+
                             </div>
                             <div class="col-md-12 mb-3">
-                           
+
                             </div>
                         </div>
                     </div>
@@ -93,25 +93,33 @@
                     <div class="col-md-6 ftco-animate">
                         <form action="#" class="contact-form">
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-12">
                                     <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="Votre Nom">
+                                        <input type="text" class="form-control" placeholder="Donne Le Nom">
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Donner Une Description">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Donner Le Prix">
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">                  <input type="text" class="form-control" placeholder="Choisir Un Fichier">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="Votre Email">
+                                        <button class="btn btn-primary py-3 px-5">Choisir</button>
                                     </div>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Sujet">
-                            </div>
-                            <div class="form-group">
-                                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <input type="submit" value="Envoyer" class="btn btn-primary py-3 px-5">
+                                <input type="submit" value="Ajouter" class="btn btn-primary py-3 px-5">
                             </div>
                         </form>
                     </div>
@@ -123,43 +131,42 @@
 
 
         <footer class="ftco-footer ftco-section img">
-            <div class="overlay"></div>
-            <div class="container">
-               
-                <div class="row">
-                    <div class="col-md-12 text-center">
 
-                        <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-                    </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+
+                    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                 </div>
             </div>
-        </footer>
+        </div>
+    </footer>
 
 
 
-        <!-- loader -->
-        <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-        <script src="js/jquery.min.js"></script>
-        <script src="js/jquery-migrate-3.0.1.min.js"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery.easing.1.3.js"></script>
-        <script src="js/jquery.waypoints.min.js"></script>
-        <script src="js/jquery.stellar.min.js"></script>
-        <script src="js/owl.carousel.min.js"></script>
-        <script src="js/jquery.magnific-popup.min.js"></script>
-        <script src="js/aos.js"></script>
-        <script src="js/jquery.animateNumber.min.js"></script>
-        <script src="js/bootstrap-datepicker.js"></script>
-        <script src="js/jquery.timepicker.min.js"></script>
-        <script src="js/scrollax.min.js"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-        <script src="js/google-map.js"></script>
-        <script src="js/main.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script src="js/jquery.timepicker.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    <script src="js/google-map.js"></script>
+    <script src="js/main.js"></script>
 
-    </body>
+</body>
 </html>
+
