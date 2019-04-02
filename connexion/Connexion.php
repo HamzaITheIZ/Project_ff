15c4e66660a1330263c635257db79fc4b3d202e2
watashiwa L desu <watashiwa L desu@DESKTOP-RVGD8P5>
2019-04-02 14:59:43 +0100

    cnx

---
+++ connexion/Connexion.php
@@ -0,0 +1,29 @@
+<?php
+
+class Connexion
+{
+    public $connexion;    
+    //private $connexion;
+
+    function __construct()
+    {
+        $username = "root";
+        $password = "";
+        $dsname = "mysql:host=localhost;dbname=project_ff";
+
+        try{
+            $this->connexion = new PDO($dsname,$username,$password);
+            $this->connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e)
+        {
+            die("erreur ! :" .$e->getMessage());
+        }
+    }
+    function getConnexion()
+    {
+        return $this->connexion;
+    }
+}
+
+?>
