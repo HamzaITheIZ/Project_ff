ce3703cb82469488a29b358fa536cd76556e5159
Hamza The Z <Hamza The Z@HamzaTheZ-PC>
2019-04-02 14:16:25 +0100

    a lot of change

--- js/user.js
+++ js/user.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
         var namestat = false;
         var emailstat = false;
         var passwordstat = false;
-        var name = ("#user_name");
-        var email = ("#user_email");
-        var password = ("#user_pssword");
-        var password2 = ("#pass2");
+        var name = $("#user_name");
+        var email = $("#user_email");
+        var password = $("#user_pssword");
+        var password2 = $("#pass2");
         
         var e_patt = new RegExp(/^[a-z0-9_-]+(\.[a-z0-9_-]+)*@[a-z0-9_-]+(\.[a-z0-9_-]+)*(\.[a-z]{2,4})$/);
         
         if (name.val() == "" || name.val().length < 6) {
-            name.addClass("border-danger");
-            $("#u_error").html("<span class='text-danger'>S'il vous plaît entrer le nom et le nom doit être plus de 6 caractères</span>");
+            name.addClass("border-dark");
+            $("#u_error").html("<span class='text-dark'>S'il vous plaît entrer le nom et le nom doit être plus de 6 caractères</span>");
             namestat = false;
         } else {
             name.removeClass("border-danger");
